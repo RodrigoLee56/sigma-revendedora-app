6e58904243442ffa274fd52ad35e89bed7131aa8
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-16 18:19:37 -0400

    feat:(cliente) Adiciona Annotation @Table na classe Cliente

--- src/main/java/com/rede/sigma/domain/Cliente/Cliente.java
+++ src/main/java/com/rede/sigma/domain/Cliente/Cliente.java
@@ -2,6 +2,7 @@ package com.rede.sigma.domain.Cliente;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "clientes")
 public class Cliente {
 
 	@Id
