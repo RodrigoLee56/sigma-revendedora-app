0d5b10a3055f0644aafbf1c2b7b78e072f98d1e9
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-17 02:40:58 -0400

    feat(montadoraRepository): Criação da interface MontadoraRepository

---
+++ src/main/java/com/rede/sigma/repository/MontadoraRepository.java
@@ -0,0 +1,9 @@
+package com.rede.sigma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rede.sigma.domain.Montadora.Montadora;
+
+public interface MontadoraRepository extends JpaRepository<Montadora, String> {
+
+}
