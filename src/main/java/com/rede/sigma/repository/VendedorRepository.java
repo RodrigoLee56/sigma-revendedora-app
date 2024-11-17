06b39ff066f71238fd33dc4a311b7b0a29f96490
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-17 02:34:56 -0400

    feat(vendedorRepository): Criação da interface VendedorRepository

---
+++ src/main/java/com/rede/sigma/repository/VendedorRepository.java
@@ -0,0 +1,9 @@
+package com.rede.sigma.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rede.sigma.domain.Vendedor.Vendedor;
+
+public interface VendedorRepository extends JpaRepository<Vendedor, Integer> {
+
+}
