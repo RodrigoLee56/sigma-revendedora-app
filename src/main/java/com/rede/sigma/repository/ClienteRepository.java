c699cc1884636e3647491476d9b93f9c956d04c2
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-20 22:05:53 -0400

    feat(clienteRepository): Adiciona método para listar clientes pelo nome

--- src/main/java/com/rede/sigma/repository/ClienteRepository.java
+++ src/main/java/com/rede/sigma/repository/ClienteRepository.java
@@ -1,9 +1,13 @@
 package com.rede.sigma.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.rede.sigma.domain.Cliente.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, String> {
-
+	@Query("SELECT c FROM Cliente c ORDER BY c.nome ASC")
+	List<Cliente> findAllOrderedByName();
 }
