181b1361aa7c5f6a747487eeb648708f042b628e
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-21 03:52:53 -0400

    feat(vendedorServiceImpl): Criação da classe VendedorServiceImpl

---
+++ src/main/java/com/rede/sigma/service/impl/VendedorServiceImpl.java
@@ -0,0 +1,49 @@
+package com.rede.sigma.service.impl;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.rede.sigma.domain.Vendedor.Vendedor;
+import com.rede.sigma.service.VendedorService;
+
+public class VendedorServiceImpl implements VendedorService {
+
+	@Override
+	public Vendedor salvar(Vendedor vendedor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vendedor atualizar(Integer codigo, Vendedor vendedor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(Integer codigo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor buscarPorCodigo(Integer codigo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Vendedor> listarPaginado(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
