51a917b47570ef2af26b781cadb3f77bf8cc4fc6
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-21 03:25:01 -0400

    refactor(clienteService): Alteração dos tipos dos parâmetros dos métodos (atualizar, deletar e buscarPorCpf). Adicionação do método listarPaginado

--- src/main/java/com/rede/sigma/service/ClienteService.java
+++ src/main/java/com/rede/sigma/service/ClienteService.java
@@ -2,6 +2,8 @@ package com.rede.sigma.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.rede.sigma.domain.Cliente.Cliente;
@@ -11,13 +13,15 @@ public interface ClienteService {
 
 	Cliente salvar(Cliente cliente);
 
-	Cliente atualizar(Long id, Cliente cliente);
+	Cliente atualizar(String cpf, Cliente cliente);
 
-	void deletar(Long id);
+	void deletar(String cpf);
 
-	Cliente buscarPorId(Long id);
+	Cliente buscarPorCpf(String cpf);
 
 	List<Cliente> listarTodos();
+
+	Page<Cliente> listarPaginado(Pageable pageable);
 
 	List<Cliente> listarOrdenadosPorNome();
 
