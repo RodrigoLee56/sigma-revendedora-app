ae68cba57e2501cd5cdb6c30092d30332b984c47
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-21 02:16:59 -0400

    refactor(clienteService) Alteração o arquivo de classe para interface, e add os métodos (salvar, atualizar, deletar, buscarPorId e listarTodos)

---
+++ src/main/java/com/rede/sigma/service/ClienteService.java
@@ -0,0 +1,22 @@
+package com.rede.sigma.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.rede.sigma.domain.Cliente.Cliente;
+
+@Service
+public interface ClienteService {
+
+	Cliente salvar(Cliente cliente);
+
+	Cliente atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+	Cliente buscarPorId(Long id);
+
+	List<Cliente> listarTodos();
+
+}
