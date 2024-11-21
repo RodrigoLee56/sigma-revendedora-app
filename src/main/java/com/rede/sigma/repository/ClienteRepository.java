eb4c3ba01cbf417003816093ba306d0446a46903
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-21 03:35:21 -0400

    feat(clienteRepository): Crianção do método findAll

--- src/main/java/com/rede/sigma/repository/ClienteRepository.java
+++ src/main/java/com/rede/sigma/repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package com.rede.sigma.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +12,7 @@ import com.rede.sigma.domain.Cliente.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, String> {
 	@Query("SELECT c FROM Cliente c ORDER BY c.nome ASC")
 	List<Cliente> findAllOrderedByName();
+
+	@Override
+	Page<Cliente> findAll(Pageable pageable);
 }
