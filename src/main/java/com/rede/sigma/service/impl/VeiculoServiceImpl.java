e1f527da1fefd59be39bd16916e0edb5800562c6
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-25 11:15:58 -0400

    feat(veiculoServiceImpl): Criação da classe VeiculoServiceImpl e seus métodos

---
+++ src/main/java/com/rede/sigma/service/impl/VeiculoServiceImpl.java
@@ -0,0 +1,79 @@
+package com.rede.sigma.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.rede.sigma.domain.Veiculo.Veiculo;
+import com.rede.sigma.repository.VeiculoRepository;
+import com.rede.sigma.service.VeiculoService;
+
+@Service
+public class VeiculoServiceImpl implements VeiculoService {
+	
+	@Autowired
+	private VeiculoRepository veiculoRepository;
+
+	@Override
+	public Veiculo salvar(Veiculo veiculo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Veiculo atualizar(String numeroChassi, Veiculo veiculo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deletar(String numeroChassi) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Veiculo buscarPorNumeroChassi(String numeroChassi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Veiculo> listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Veiculo> listarPaginado(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Veiculo> listarPorMarca(String marca) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Veiculo> listarPorMarcaPaginado(String marca, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Veiculo> listarPorAnoFabricacaoPaginado(Integer anoFabricacao, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Veiculo> listarPorCorPaginado(String cor, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
