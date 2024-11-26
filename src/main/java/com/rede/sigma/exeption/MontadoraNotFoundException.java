d424e04e319280587235b51f368ca0d8beed2976
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-26 13:02:49 -0400

    feat(montadoraNotFoundException): Criação da exception MontadoraNotFoundException

---
+++ src/main/java/com/rede/sigma/exeption/MontadoraNotFoundException.java
@@ -0,0 +1,11 @@
+package com.rede.sigma.exeption;
+
+public class MontadoraNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public MontadoraNotFoundException(String message) {
+		super(message);
+	}
+
+}
