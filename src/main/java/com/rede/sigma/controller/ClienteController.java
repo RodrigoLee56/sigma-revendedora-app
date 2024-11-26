8684bbfbe4f6ff3f9ef33effe484d206eb01ce52
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-26 18:45:05 -0400

    feat(clienteController): Criação e implementação do método listarTodosClientes

--- src/main/java/com/rede/sigma/controller/ClienteController.java
+++ src/main/java/com/rede/sigma/controller/ClienteController.java
@@ -1,5 +1,7 @@
 package com.rede.sigma.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,4 +35,10 @@ public class ClienteController {
 	public Cliente buscarClientePorCpf(@PathVariable String cpf) {
 		return clienteService.buscarPorCpf(cpf);
 	}
+
+	@GetMapping
+	public List<Cliente> listarTodosClientes() {
+		return clienteService.listarTodos();
+	}
+
 }
