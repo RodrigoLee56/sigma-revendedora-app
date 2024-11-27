496d8912163c5360f570818c1d53559284d5db71
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-27 10:33:54 -0400

    feat(vendedorController): Criação e implementação do método listarVendedores

--- src/main/java/com/rede/sigma/controller/VendedorController.java
+++ src/main/java/com/rede/sigma/controller/VendedorController.java
@@ -1,16 +1,28 @@
 package com.rede.sigma.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rede.sigma.domain.Vendedor.Vendedor;
 import com.rede.sigma.service.VendedorService;
 
-@RestController
+@Controller
 @RequestMapping("/vendedores")
 public class VendedorController {
-	
+
 	@Autowired
-    private VendedorService vendedorService;
-	
+	private VendedorService vendedorService;
+
+	@GetMapping
+	public String listarVendedores(Model model) {
+		List<Vendedor> vendedores = vendedorService.listarTodos();
+		model.addAttribute("vendedores", vendedores);
+		return "vendedores/listar-vendedores";
+	}
+
 }
