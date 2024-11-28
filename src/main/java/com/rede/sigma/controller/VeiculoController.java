68f1757609d4f166e90a0b653cc8665f56b39472
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-27 23:54:17 -0400

    feat(veiculoController): Criação da classe VeiculoController e com seu atributo

--- src/main/java/com/rede/sigma/controller/VeiculoController.java
+++ src/main/java/com/rede/sigma/controller/VeiculoController.java
@@ -1,14 +1,9 @@
 package com.rede.sigma.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.rede.sigma.domain.Veiculo.Veiculo;
 import com.rede.sigma.service.VeiculoService;
 
 @Controller
@@ -18,10 +13,4 @@ public class VeiculoController {
 	@Autowired
 	private VeiculoService veiculoService;
 
-	@GetMapping
-	public String listarVeiculos(Model model) {
-		List<Veiculo> veiculos = veiculoService.listarTodos();
-		model.addAttribute("veiculos", veiculos);
-		return "veiculos/listar-veiculos";
-	}
 }
