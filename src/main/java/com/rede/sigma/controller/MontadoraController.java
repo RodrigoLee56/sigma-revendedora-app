4ab273c2ff44d3d46ee65ff27c9091ea3914472e
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-29 01:54:51 -0400

    feat(montadoraController):Criação e implementação do método editarMontadora

--- src/main/java/com/rede/sigma/controller/MontadoraController.java
+++ src/main/java/com/rede/sigma/controller/MontadoraController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,5 +37,12 @@ public class MontadoraController {
     public String salvarMontadora(@ModelAttribute Montadora montadora) {
         montadoraService.salvar(montadora);
         return "redirect:/montadoras";
+    }
+	
+	@GetMapping("/editar/{cnpj}")
+    public String editarMontadora(@PathVariable String cnpj, Model model) {
+        Montadora montadora = montadoraService.buscarPorCnpj(cnpj);
+        model.addAttribute("montadora", montadora);
+        return "montadoras/form-montadora";
     }
 }
