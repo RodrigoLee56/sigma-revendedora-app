de8a92d9d230d958d97cb037a418d4990e946ccb
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-29 02:46:52 -0400

    feat(operacaoController): Criação e implementação do método listarOperacoes

--- src/main/java/com/rede/sigma/controller/OperacaoController.java
+++ src/main/java/com/rede/sigma/controller/OperacaoController.java
@@ -1,9 +1,14 @@
 package com.rede.sigma.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.rede.sigma.domain.Operacao.Operacao;
 import com.rede.sigma.service.ClienteService;
 import com.rede.sigma.service.MontadoraService;
 import com.rede.sigma.service.OperacaoService;
@@ -27,4 +32,11 @@ public class OperacaoController {
 
 	@Autowired
 	private MontadoraService montadoraService;
+
+	@GetMapping
+	public String listarOperacoes(Model model) {
+		List<Operacao> operacoes = operacaoService.listarTodas();
+		model.addAttribute("operacoes", operacoes);
+		return "operacoes/listar-operacoes";
+	}
 }
