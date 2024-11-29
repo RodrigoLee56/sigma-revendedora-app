bbb6a66b69513583f5a3b0e087edafe6e729fbf1
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-28 23:39:06 -0400

    feat(veiculoController): Criação e implementação dos métodos editarVeiculo

--- src/main/java/com/rede/sigma/controller/VeiculoController.java
+++ src/main/java/com/rede/sigma/controller/VeiculoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,5 +38,12 @@ public class VeiculoController {
     public String salvarVeiculo(@ModelAttribute Veiculo veiculo) {
         veiculoService.salvar(veiculo);
         return "redirect:/veiculos";
+    }
+	
+	@GetMapping("/editar/{numeroChassi}")
+    public String editarVeiculo(@PathVariable String numeroChassi, Model model) {
+        Veiculo veiculo = veiculoService.buscarPorNumeroChassi(numeroChassi);
+        model.addAttribute("veiculo", veiculo);
+        return "veiculos/form-veiculo";
     }
 }
