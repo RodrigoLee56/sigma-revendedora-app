34a40f3fd55a66279a9f2c1189b9a944662058ec
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-29 02:48:14 -0400

    feat(operacaoController): Criação e implementação do método salvarOperacao

--- src/main/java/com/rede/sigma/controller/OperacaoController.java
+++ src/main/java/com/rede/sigma/controller/OperacaoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rede.sigma.domain.Operacao.Operacao;
@@ -50,7 +52,12 @@ public class OperacaoController {
         return "operacoes/form-operacao";
     }
 	
-	
+	@PostMapping("/salvar")
+    public String salvarOperacao(@ModelAttribute Operacao operacao) {
+        operacaoService.salvar(operacao);
+        return "redirect:/operacoes";
+    }
+
 	
 	
 	
