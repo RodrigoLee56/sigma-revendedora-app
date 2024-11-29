e16fa818b52fc9cdd0d3bcc36a93cf67f397b4a9
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-29 01:40:25 -0400

    feat(montadoraController):Criação e implementação do método listarMontadoras

--- src/main/java/com/rede/sigma/controller/MontadoraController.java
+++ src/main/java/com/rede/sigma/controller/MontadoraController.java
@@ -1,10 +1,26 @@
 package com.rede.sigma.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rede.sigma.domain.Montadora.Montadora;
+import com.rede.sigma.service.MontadoraService;
 
 @Controller
 @RequestMapping("/montadoras")
 public class MontadoraController {
-	
+	@Autowired
+	private MontadoraService montadoraService;
+
+	@GetMapping
+	public String listarMontadoras(Model model) {
+		List<Montadora> montadoras = montadoraService.listarTodas();
+		model.addAttribute("montadoras", montadoras);
+		return "montadoras/listar-montadoras";
+	}
 }
