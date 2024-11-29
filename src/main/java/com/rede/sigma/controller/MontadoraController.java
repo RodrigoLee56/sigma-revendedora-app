48b09b1d15cc397269531d4ea1ec6f58a520625c
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-29 01:50:55 -0400

    feat(montadoraController):Criação e implementação do método salvarMontadora

--- src/main/java/com/rede/sigma/controller/MontadoraController.java
+++ src/main/java/com/rede/sigma/controller/MontadoraController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rede.sigma.domain.Montadora.Montadora;
@@ -28,5 +30,11 @@ public class MontadoraController {
     public String novaMontadora(Model model) {
         model.addAttribute("montadora", new Montadora());
         return "montadoras/form-montadora";
+    }
+	
+	@PostMapping("/salvar")
+    public String salvarMontadora(@ModelAttribute Montadora montadora) {
+        montadoraService.salvar(montadora);
+        return "redirect:/montadoras";
     }
 }
