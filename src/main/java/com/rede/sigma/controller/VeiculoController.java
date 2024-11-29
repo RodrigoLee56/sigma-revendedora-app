268a2febd3c9ac61d0493b075a8823d2157ef03d
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-28 23:43:25 -0400

    feat(veiculoController): Criação e implementação dos métodos verVeiculo

--- src/main/java/com/rede/sigma/controller/VeiculoController.java
+++ src/main/java/com/rede/sigma/controller/VeiculoController.java
@@ -27,35 +27,42 @@ public class VeiculoController {
 		model.addAttribute("veiculos", veiculos);
 		return "veiculos/listar-veiculos";
 	}
-	
+
 	@GetMapping("/novo")
-    public String novoVeiculo(Model model) {
-        model.addAttribute("veiculo", new Veiculo());
-        return "veiculos/form-veiculo";
-    }
-	
+	public String novoVeiculo(Model model) {
+		model.addAttribute("veiculo", new Veiculo());
+		return "veiculos/form-veiculo";
+	}
+
 	@PostMapping("/salvar")
-    public String salvarVeiculo(@ModelAttribute Veiculo veiculo) {
-        veiculoService.salvar(veiculo);
-        return "redirect:/veiculos";
-    }
-	
+	public String salvarVeiculo(@ModelAttribute Veiculo veiculo) {
+		veiculoService.salvar(veiculo);
+		return "redirect:/veiculos";
+	}
+
 	@GetMapping("/editar/{numeroChassi}")
-    public String editarVeiculo(@PathVariable String numeroChassi, Model model) {
-        Veiculo veiculo = veiculoService.buscarPorNumeroChassi(numeroChassi);
-        model.addAttribute("veiculo", veiculo);
-        return "veiculos/form-veiculo";
-    }
-	
+	public String editarVeiculo(@PathVariable String numeroChassi, Model model) {
+		Veiculo veiculo = veiculoService.buscarPorNumeroChassi(numeroChassi);
+		model.addAttribute("veiculo", veiculo);
+		return "veiculos/form-veiculo";
+	}
+
 	@PostMapping("/atualizar")
-    public String atualizarVeiculo(@ModelAttribute Veiculo veiculo) {
-        veiculoService.atualizar(veiculo.getNumeroChassi(), veiculo);
-        return "redirect:/veiculos";
-    }
-	
+	public String atualizarVeiculo(@ModelAttribute Veiculo veiculo) {
+		veiculoService.atualizar(veiculo.getNumeroChassi(), veiculo);
+		return "redirect:/veiculos";
+	}
+
 	@GetMapping("/deletar/{numeroChassi}")
-    public String deletarVeiculo(@PathVariable String numeroChassi) {
-        veiculoService.deletar(numeroChassi);
-        return "redirect:/veiculos";
-    }
+	public String deletarVeiculo(@PathVariable String numeroChassi) {
+		veiculoService.deletar(numeroChassi);
+		return "redirect:/veiculos";
+	}
+
+	@GetMapping("/{numeroChassi}")
+	public String verVeiculo(@PathVariable String numeroChassi, Model model) {
+		Veiculo veiculo = veiculoService.buscarPorNumeroChassi(numeroChassi);
+		model.addAttribute("veiculo", veiculo);
+		return "veiculos/ver-veiculo";
+	}
 }
