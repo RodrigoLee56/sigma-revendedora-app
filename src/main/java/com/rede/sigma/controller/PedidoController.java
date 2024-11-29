07a43610dc5a3e4ae870d6c3a22faa2f87d4f565
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2024-11-29 02:20:19 -0400

    feat(pedidoController): Criação e implementação do método novoPedido

--- src/main/java/com/rede/sigma/controller/PedidoController.java
+++ src/main/java/com/rede/sigma/controller/PedidoController.java
@@ -1,9 +1,14 @@
 package com.rede.sigma.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.rede.sigma.domain.Pedido.Pedido;
 import com.rede.sigma.service.ClienteService;
 import com.rede.sigma.service.MontadoraService;
 import com.rede.sigma.service.PedidoService;
@@ -23,4 +28,20 @@ public class PedidoController {
 
 	@Autowired
 	private MontadoraService montadoraService;
+	
+	@GetMapping
+    public String listarPedidos(Model model) {
+        List<Pedido> pedidos = pedidoService.listarTodos();
+        model.addAttribute("pedidos", pedidos);
+        return "pedidos/listar-pedidos";
+    }
+	
+	@GetMapping("/novo")
+    public String novoPedido(Model model) {
+        model.addAttribute("pedido", new Pedido());
+        model.addAttribute("clientes", clienteService.listarTodos());
+        model.addAttribute("vendedores", vendedorService.listarTodos());
+        model.addAttribute("montadoras", montadoraService.listarTodas());
+        return "pedidos/form-pedido";
+    }
 }
